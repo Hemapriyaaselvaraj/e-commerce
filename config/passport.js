e542a92fecb4a1ace90239460f5720afd9fd9dc3
Hemapriyaaselvaraj <hemapriyaaselvaraj@gmail.com>
2026-01-14 09:24:31 +0530

    change port

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/user/auth/google/callback", 
+    callbackURL: process.env.GOOGLE_CALLBACK_URI, 
     passReqToCallback: true
   },
   async (req, accessToken, refreshToken, profile, done) => {
--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
